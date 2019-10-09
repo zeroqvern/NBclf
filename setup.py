60cd1db6096d2bf6b6ad6c1b486b3d6b13094495
AshVern <45629815+zeroqvern@users.noreply.github.com>
2019-10-09 20:52:41 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,82 +5,82 @@ import pickle
 import numpy as np
 import json
 import pandas as pd
+from preProcessTweets import PreProcessTweets
 
-
-
-#app
 app = Flask(__name__)
 api = Api(app)
 
-#load NBModel
-# model = MultinomialNB()
+# load NBModel
 with open('model.pkl', 'rb') as training_model:
     model = pickle.load(training_model)
 
 
-
-# # argument parsing
-# parser = reqparse.RequestParser()
-# parser.add_argument('query')
-
-#routes
+# routes
 @app.route('/', methods=['POST'])
-
 def begin():
     # get data (in list, dict, str format)
     req = request.get_json(force=True)
-    # print("type: {}".format(type(data)))
+
     tweetList = []
     data = req
-    if (isinstance(req, list)):
+
+    # convert request data to string
+    if isinstance(req, list):
         print("list")
         req = str(req[0])
         data = req.replace("'", "\"")
-    elif (isinstance(req, dict)):
+    elif isinstance(req, dict):
         data = json.dumps(req)
-        print (data)
+        print(data)
 
+    # store strings to list
     p = json.loads(data)
     for i in p['tweet']:
         tweetList.append(i)
 
-    #prediction
+    # prediction
     output = prediction(tweetList)
 
+    # print output and return to requester
     print(output)
-    # output =  {'Overall Sentiment': str(1), 'Positive': 1, 'Negative': 1}
     return jsonify(output)
 
-def prediction (tweetList):
 
+def prediction(tweetList):
     pos = 0
     neg = 0
 
+    # clean tweet texts in list
+    proc = PreProcessTweets()
+    cleanListSet = proc.cleanTweetSet(tweetList)
+
     # predictions
-    result = model.predict(tweetList)
-    print(result)
+    result = model.predict(cleanListSet)
 
+    # count number of negative and positive tweets
     for i in result:
-        if (i == 0): neg = neg + 1
-        else: pos = pos + 1
+        if i == 0:
+            neg = neg + 1
+        else:
+            pos = pos + 1
 
+    # calculate average overall sentiment
     avg = avgSentiment(result)
 
-    #json output
+    # json output
     output = {'Overall Sentiment': str(avg), 'Positive': pos, 'Negative': neg}
-    return  output
+    return output
 
-def avgSentiment (allSentiment):
 
+def avgSentiment(allSentiment):
     sumSentiment = sum(allSentiment)
     tweetNum = len(allSentiment)
 
-    avg = sumSentiment/tweetNum
+    avg = sumSentiment / tweetNum
     avg = round(avg, 2)
 
     return avg
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
