e36503b9cc6c2c944be04f206656e5d1362c2ee5
AshVern <45629815+zeroqvern@users.noreply.github.com>
2019-10-19 14:10:35 +0800

    Added database

--- configHeroku.py
+++ configHeroku.py
@@ -7,21 +7,29 @@ CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
 CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
 ACCESS_KEY = os.environ.get('ACCESS_KEY')
 ACCESS_SECRET = os.environ.get('ACCESS_SECRET')
+MONGODB = os.environ.get('MONGODB')
 auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
-
+# twitter authentication
 def authentication():
     try:
         api = tweepy.API(auth)
         print("Authentication OK")
         return api
     except:
-        print("Error during authenticatoin")
+        print("Error during authentication")
         return None
 
+# MongoDB authentication
+def dbConnection():
+    try:
+        client = MongoClient(MONGODB)
+        print("Connected to database")
+        return client
 
-# test authentication
-# print(twitter_api.VerifyCredentials())
+    except:
+        print("Error connecting to database")
+
 
 
