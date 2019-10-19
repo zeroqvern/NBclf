1e75dfc5be619abea76f0c42786277ff67c19064
AshVern <45629815+zeroqvern@users.noreply.github.com>
2019-10-19 14:22:10 +0800

    Update tweetManager.py

--- tweetManager.py
+++ tweetManager.py
@@ -4,19 +4,8 @@ from preProcessTweets import PreProcessTweets
 
 
 def begin(keyword):
-    result = getTweets(keyword)
-
-    for tweet in result:
-        print(tweet)
-
-    return result
-
-
-def getTweets(keyword):
     api = authentication()
-
     keyword = keyword + " -filter:retweets"
-
-    # tweets_fetched = twitter_api.GetSearch(search_keyword, count=10)
     tweets = tweepy.Cursor(api.search, q=keyword, result_type="recent", lang="en").items(5)
-    return [tweet.text for tweet in tweets]
+
+    return tweets
