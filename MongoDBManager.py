4dda5a665f4c6af3e260f7886443ab99f54047c2
AshVern <45629815+zeroqvern@users.noreply.github.com>
2019-10-19 14:08:24 +0800

    Database

---
+++ MongoDBManager.py
@@ -0,0 +1,35 @@
+from configHeroku import dbConnection
+
+
+def MongoDB(userTweets, sentiment, userID):
+    # Connecting to database
+    client = dbConnection()
+    print(client)
+
+    # get database
+    db = client.get_database('TweetFeel')
+    tweetCollection = db.Raw_Tweets
+
+    tweetData = []
+    print("here")
+    i = 0
+    for tweet in userTweets:
+        t = {"userid": userID,
+             "name": tweet.user.name,
+             "created at": tweet.created_at,
+             "text": tweet.text,
+             "sentiment": str(sentiment[i])}
+        tweetData.append(t)
+
+        print("name:", tweet.user.name)
+        print("created at: ", tweet.created_at)
+        print("text: ", tweet.text)
+        print(sentiment[i])
+        print("")
+
+        i = i + 1
+
+    # for tw in tweetData:
+    #     print(tw)
+
+    tweetCollection.insert_many(tweetData)
